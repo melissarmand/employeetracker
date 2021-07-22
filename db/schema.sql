942318daae68dfbfc6029788ae99ada9f8f38315
Melissa Armand <melissarmand@gmail.com>
2021-07-22 06:45:22 -0400

    completed schema file with the required tables for department, role, and employee

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,30 @@
 DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department (
+    id INT NOT NULL,
+    department_name VARCHAR(30) NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employeeRole (
+    id INT NOT NULL,
+    title VARCHAR(30) NULL,
+    salary DECIMAL (10,4) NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NULL,
+    last_name VARCHAR(30) NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+
+);
