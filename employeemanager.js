8ad9bad0c303c913211922ac3d4929898b227cc6
Melissa Armand <melissarmand@gmail.com>
2021-07-30 12:02:16 -0400

    Added most of the questions to prompt

--- employeemanager.js
+++ employeemanager.js
@@ -1,7 +1,18 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const sequelize = require('./config/connection');
-const { allowedNodeEnvironmentFlags } = require('process');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password1234',
+    database: 'employee_db',
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    runChoices();
+});
 
 const runChoices = () => {
 
@@ -52,4 +63,150 @@ const runChoices = () => {
 
         }
     });
+};
+
+const employeeDisplay = () => {
+    inquirer
+        .prompt({
+            name:'employee',
+            type:'confirm',
+            message: 'View all employees now?',
+        })
+        .then((answer) => {
+            const query = 'SELECT first_name, last_name, role_id, manager_id FROM employee WHERE ?';
+            connection.query(query, {})
+        })
+        runChoices();
+};
+
+const departmentDisplay = () => {
+    inquirer
+        .prompt({
+            name: 'department',
+            type: 'confirm',
+            message: 'View all departments now?',
+        })
+        .then((answer) => {
+            const query = 'SELECT '
+        });
+
+        runChoices();
+};
+
+const roleDisplay = () => {
+    inquirer    
+        .prompt({
+            name: 'role',
+            type: 'confirm',
+            message: 'View all departments now?',
+        })
+        .then((answer) => {
+            const query = 'SELECT '
+        });
+        runChoices();
+};
+
+const addEmployee = () => {
+    inquirer
+        .prompt([
+         {
+            name: 'firstname',
+            type: 'input',
+            message: 'What is the employees first name?',
+            
+        },
+        {
+            name: 'lastname',
+            type: 'input',
+            message: 'What is the employees last name?',
+            
+
+        },
+        {
+            name: 'manager',
+            type: 'input',
+            message: 'Who is the employees manager?',
+            validate(value) {
+                if (isNaN(value) === false) {
+                    return true;
+              }
+              return false;
+                }, 
+
+        },
+        {
+            name: 'role',
+            type: 'input',
+            message: 'What is the employees role?',
+            validate(value) {
+            if (isNaN(value) === false) {
+                return true;
+          }
+          return false;
+            }, 
+
+        },     
+        ])
+        .then((answer) => {
+            connection.query('INSERT INTO employee SET ?',
+            {
+                first_name: answer.firstname,
+                last_name: answer.lastname,
+                manager_id: answer.manager,
+                role_id: answer.role
+            },
+            runChoices()
+            )
+
+        });
+
 }
+const addDepartment = () => {
+    inquirer
+    .prompt({
+        name: 'department',
+        type: 'input',
+        message: 'What is the name of the new department?'
+    })
+
+    .then((answer) => {
+        connection.query('INSERT INTO department SET ?',
+        {
+            department_name: answer.department,
+        },
+        runChoices()
+        )
+    })
+};
+
+const addRole = () => {
+    inquirer
+        .prompt([
+        {
+            name: 'title',
+            type: 'input',
+            message: 'What is the title of this role?',
+
+        },
+
+        {
+            name: 'salary',
+            type: 'input',
+            message: 'What is the salary for this role?',
+            validate(value) {
+                if (isNaN(value) === false) {
+                    return true;
+              }
+              return false;
+                }, 
+
+        },
+
+        {
+            name: 'department',
+            type:'input',
+            message: 'What is the department id?'
+        },
+        ])
+}
+
